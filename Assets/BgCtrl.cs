6d8a6647cbff4987f74a755a592de568f95d1466
Gongdolgun <159753min@gmail.com>
2022-10-16 22:45:08 +0900

    Last Asset Commit

---
+++ Assets/BgCtrl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgCtrl : MonoBehaviour
+{
+    public GameObject front;
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(600);
+            if (front.activeSelf) front.SetActive(false);
+            else front.SetActive(true);
+        }
+    }
+
+     
+}
