10cdbd8735a5949c8093d69991e0fcd91d1d29a8
Gongdolgun <159753min@gmail.com>
2022-10-16 22:36:45 +0900

    Asset Commit 2
    Gizmos / kimgu / kimgu_test

---
+++ Assets/kimgu/scripts/command.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class command : MonoBehaviour
+{
+    public Animator animator;
+    public AudioClip ac;
+    public AudioClip ac1;
+    public AudioClip ac2;
+    public AudioClip ac3;
+    public AudioSource audioSource;
+    public void SetTrigger(string _s)
+    {
+        Debug.Log(_s);
+        animator.SetTrigger(_s);
+        ac =  Resources.Load(_s) as AudioClip;
+        if(ac)audioSource.PlayOneShot(ac);
+
+    }
+}
